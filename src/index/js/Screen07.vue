95bfff68a8662f334dc1622d87d9d795dfbf837b
djkong <djkong@corp.netease.com>
2019-04-16 22:37:14 +0800

    screen07

--- src/index/js/Screen07.vue
+++ src/index/js/Screen07.vue
@@ -3,62 +3,64 @@
         <div class="main ps-144">
             <div class="type0 ps-146">
                 <div class="text3 ps-204">
-                    <div class="text ps-19"></div>
-                    <div class="flag ps-63"></div>
+                    <div class="text ps-19" :style="type0Text3Style"></div>
+                    <div class="flag ps-63" :style="type0Flag3Style"></div>
                 </div>
                 <div class="text2 ps-216">
-                    <div class="text ps-209"></div>
-                    <div class="flag ps-215"></div>
+                    <div class="text ps-209" :style="type0Text2Style"></div>
+                    <div class="flag ps-215" :style="type0Flag2Style"></div>
                 </div>
                 <div class="text1 ps-227">
-                    <div class="text ps-220"></div>
-                    <div class="flag ps-226"></div>
+                    <div class="text ps-220" :style="type0Text1Style"></div>
+                    <div class="flag ps-226" :style="type0Flag1Style"></div>
                 </div>
                 <div class="text0 ps-238">
-                    <div class="text ps-231"></div>
-                    <div class="flag ps-237"></div>
+                    <div class="text ps-231" :style="type0Text0Style"></div>
+                    <div class="flag ps-237" :style="type0Flag0Style"></div>
                 </div>
-                <div class="title ps-154"></div>
+                <div class="title ps-154" :style="main0titleStyle"></div>
             </div>
             <div class="type1 ps-148">
                 <div class="data1 ps-184">
                     <div class="main ps-191">
+                        <span class="bg" :style="data1BgStyle"></span>
                         <div class="date ps-200"></div>
-                        <canvas class="canvas pie ps-195" ref="canvas1"></canvas>
-                        <div class="text ps-198"></div>
-                        <div class="title ps-193"></div>
+                        <canvas class="canvas pie ps-195" ref="canvas1" :style="data1PieStype"></canvas>
+                        <div class="text ps-198" :style="data1TextStype"></div>
+                        <div class="title ps-193" :style="data1TitleStype"></div>
                     </div>
                     <div class="desc ps-186">
-                        <div class="text1 ps-188"></div>
-                        <div class="text0 ps-190"></div>
+                        <div class="text1 ps-188" :style="data1Desc1Stype"></div>
+                        <div class="text0 ps-190" :style="data1Desc0Stype"></div>
                     </div>
                 </div>
                 <div class="data0 ps-182">
                     <div class="main ps-174">
+                        <span class="bg" :style="data0BgStyle"></span>
                         <div class="date ps-176"></div>
-                        <canvas class="canvas pie ps-166" ref="canvas0"></canvas>
-                        <div class="text ps-168"></div>
-                        <div class="title ps-178"></div>
+                        <canvas class="canvas pie ps-166" ref="canvas0" :style="data0PieStype"></canvas>
+                        <div class="text ps-168" :style="data0TextStype"></div>
+                        <div class="title ps-178" :style="data0TitleStype"></div>
                     </div>
                     <div class="desc ps-158">
-                        <div class="text4 ps-165"></div>
-                        <div class="text3 ps-164"></div>
-                        <div class="text2 ps-163"></div>
-                        <div class="text1 ps-162"></div>
-                        <div class="text0 ps-161"></div>
+                        <div class="text4 ps-165" :style="data0Desc4Stype"></div>
+                        <div class="text3 ps-164" :style="data0Desc3Stype"></div>
+                        <div class="text2 ps-163" :style="data0Desc2Stype"></div>
+                        <div class="text1 ps-162" :style="data0Desc1Stype"></div>
+                        <div class="text0 ps-161" :style="data0Desc0Stype"></div>
                     </div>
                 </div>
-                <div class="title ps-152"></div>
+                <div class="title ps-152" :style="main1titleStyle"></div>
             </div>
         </div>
         <div class="summary ps-139">
-            <div class="text1 ps-143"></div>
-            <div class="text0 ps-141"></div>
+            <div class="text1 ps-143" :style="summary1Style"></div>
+            <div class="text0 ps-141" :style="summary0Style"></div>
         </div>
         <div class="title ps-133">
-            <div class="bg ps-137"></div>
-            <div class="text ps-68"></div>
-            <div class="num07 ps-135"></div>
+            <div class="bg ps-137" :style="titleBgStyle"></div>
+            <div class="text ps-68" :style="titleTextStyle"></div>
+            <div class="num07 ps-135" :style="titleNumStyle"></div>
         </div>
         </div>
 </template>
@@ -66,8 +68,320 @@
 import Pie from './lib/pie.ts';
     let pie0,pie1;
     export default {
+        props:['progress','screen'],
+        watch:{
+            progress(val){
+                this.titleNumStyle = this.$parent.tweenCss({
+                    translateX:{
+                        fromTo:[-50,50],
+                        range:[0,1.2*this.screen],
+                        progress:val,
+                    },
+                    translateY:{
+                        fromTo:[50,-100],
+                        range:[0,1.2*this.screen],
+                        progress:val,
+                    },
+                    scale:{
+                        fromTo:[1.2,.8],
+                        range:[0,1.2*this.screen],
+                        progress:val,
+                    },
+                });
+                this.titleBgStyle = this.$parent.tweenCss({
+                    translateX:{
+                        fromTo:[100,-30],
+                        range:[0,1.2*this.screen],
+                        progress:val,
+                    },
+                    translateY:{
+                        fromTo:[-100,30],
+                        range:[0,1.2*this.screen],
+                        progress:val,
+                    },
+                    rotateZ:{
+                        fromTo:[10,-10],
+                        range:[0,1.2*this.screen],
+                        progress:val,
+                    },
+                });
+                this.titleTextStyle = this.$parent.tweenCss({
+                    opacity:{
+                        fromTo:[0,1],
+                        range:[0.05,.1*this.screen], 
+                        progress:val,
+                    },
+                    translateY:{
+                        fromTo:[-30,30],
+                        range:[0,1.2*this.screen], 
+                        progress:val,
+                    },
+                    scale:{
+                        fromTo:[.3,1],
+                        range:[0.05,.2*this.screen], 
+                        progress:val,
+                        easing:'easeBackOut'
+                    }
+                });
+                this.summary0Style = this.$parent.tweenCss({
+                    opacity:{
+                        fromTo:[0,1],
+                        range:[0.12,.05*this.screen], 
+                        progress:val,
+                    },
+                    translateY:{
+                        fromTo:[50,0],
+                        range:[0.12,.1*this.screen], 
+                        progress:val,
+                        easing:'easeQuadOut'
+                    }
+                });
+                this.summary1Style = this.$parent.tweenCss({
+                    opacity:{
+                        fromTo:[0,1],
+                        range:[0.13,.05*this.screen], 
+                        progress:val,
+                    },
+                    scale:{
+                        fromTo:[1.5,1],
+                        range:[0.13,.1*this.screen], 
+                        progress:val,
+                        easing:'easeBackOut'
+                    }
+                });
+                this.main0titleStyle = this.$parent.tweenCss({
+                    opacity:{
+                        fromTo:[0,1],
+                        range:[0.8,.05*this.screen], 
+                        progress:val,
+                    },
+                    translateX:{
+                        fromTo:[-50,0],
+                        range:[0.8,.1*this.screen], 
+                        progress:val,
+                        easing:'easeBackOut'
+                    }
+                });
+                this.main1titleStyle = this.$parent.tweenCss({
+                    opacity:{
+                        fromTo:[0,1],
+                        range:[0.18,.05*this.screen], 
+                        progress:val,
+                    },
+                    translateX:{
+                        fromTo:[-50,0],
+                        range:[0.18,.1*this.screen], 
+                        progress:val,
+                        easing:'easeBackOut'
+                    }
+                });
+                for(let i = 0;i<5;i++){
+                    this[`data0Desc${i}Stype`]=this.$parent.tweenCss({
+                        opacity:{
+                            fromTo:[0,1],
+                            range:[0.2+i*.01,.05*this.screen], 
+                            progress:val,
+                        },
+                        translateX:{
+                            fromTo:[50,0],
+                            range:[0.2+i*.01,.1*this.screen], 
+                            progress:val,
+                            easing:'easeBackOut'
+                        }
+                    });
+                }
+                for(let i = 0;i<2;i++){
+                    this[`data1Desc${i}Stype`]=this.$parent.tweenCss({
+                        opacity:{
+                            fromTo:[0,1],
+                            range:[0.52+i*.01,.05*this.screen], 
+                            progress:val,
+                        },
+                        translateX:{
+                            fromTo:[50,0],
+                            range:[0.52+i*.01,.1*this.screen], 
+                            progress:val,
+                            easing:'easeBackOut'
+                        }
+                    });
+                }
+                this.data0TitleStype = this.$parent.tweenCss({
+                    opacity:{
+                        fromTo:[0,1],
+                        range:[0.35,.1*this.screen], 
+                        progress:val,
+                    },
+                    translateX:{
+                        fromTo:[100,0],
+                        range:[0.35,.25*this.screen], 
+                        progress:val,
+                        easing:'easeElastic'
+                    }
+                });
+                pie0.render({ 
+                    progress:this.$parent.tween({
+                        progress:{
+                            fromTo:[0,1],
+                            range:[.41,.1*this.screen],
+                            progress:val,
+                        },
+                    }).progress,
+                    data:this.pieData0
+                });
+                this.data0PieStype=this.$parent.tweenCss({
+                    opacity:{
+                        fromTo:[0,1],
+                        range:[.41,.05*this.screen],
+                        progress:val, 
+                    },
+                    scale:{
+                        fromTo:[.1,1],
+                        range:[.41,.1*this.screen],
+                        progress:val, 
+                        easing:'easeBackOut'
+                    },
+                });
+                this.data0TextStype=this.$parent.tweenCss({
+                    opacity:{
+                        fromTo:[0,1],
+                        range:[.43,.05*this.screen],
+                        progress:val, 
+                    },
+                    scale:{
+                        fromTo:[1.2,1],
+                        range:[.43,.1*this.screen],
+                        progress:val, 
+                        easing:'easeBackOut'
+                    },
+                });
+                
+                this.data1TitleStype = this.$parent.tweenCss({
+                    opacity:{
+                        fromTo:[0,1],
+                        range:[0.61,.1*this.screen], 
+                        progress:val,
+                    },
+                    translateX:{
+                        fromTo:[100,0],
+                        range:[0.61,.25*this.screen], 
+                        progress:val,
+                        easing:'easeElastic'
+                    }
+                });
+                pie1.render({ 
+                    progress:this.$parent.tween({
+                        progress:{
+                            fromTo:[0,1],
+                            range:[.67,.1*this.screen],
+                            progress:val,
+                        },
+                    }).progress,
+                    data:this.pieData1
+                });
+                this.data1PieStype=this.$parent.tweenCss({
+                    opacity:{
+                        fromTo:[0,1],
+                        range:[.67,.05*this.screen],
+                        progress:val, 
+                    },
+                    scale:{
+                        fromTo:[.1,1],
+                        range:[.67,.1*this.screen],
+                        progress:val, 
+                        easing:'easeBackOut'
+                    },
+                });
+                this.data1TextStype=this.$parent.tweenCss({
+                    opacity:{
+                        fromTo:[0,1],
+                        range:[.69,.05*this.screen],
+                        progress:val, 
+                    },
+                    scale:{
+                        fromTo:[1.2,1],
+                        range:[.69,.1*this.screen],
+                        progress:val, 
+                        easing:'easeBackOut'
+                    },
+                });
+                this.data0BgStyle=this.$parent.tweenCss({
+                    translateY:{
+                        fromTo:[-100,100],
+                        range:[0.3,2*this.screen],
+                        progress:val, 
+                    },
+                });
+                this.data1BgStyle=this.$parent.tweenCss({
+                    translateY:{
+                        fromTo:[-100,100],
+                        range:[0.5,2*this.screen],
+                        progress:val, 
+                    },
+                });
+                for(let i = 0;i<4;i++){
+                    this[`type0Text${i}Style`] = this.$parent.tweenCss({
+                        opacity:{
+                            fromTo:[0,1],
+                            range:[0.842+i*.035,0.05*this.screen],
+                            progress:val, 
+                        },
+                        translateX:{
+                            fromTo:[100,0],
+                            range:[0.842+i*.035,0.1*this.screen],
+                            progress:val, 
+                            easing:'easeBackOut'
+                        },
+                    });
+                    this[`type0Flag${i}Style`] = this.$parent.tweenCss({
+                        opacity:{
+                            fromTo:[0,1],
+                            range:[0.84+i*.035,0.05*this.screen],
+                            progress:val, 
+                        },
+                        translateY:{
+                            fromTo:[-30,0],
+                            range:[0.84+i*.035,0.1*this.screen],
+                            progress:val, 
+                            easing:'easeElasticOut'
+                        },
+                    });
+                }
+            }
+        },
         data(){
             return{
+                titleNumStyle:null,
+                titleTextStyle:null,
+                titleBgStyle:null,
+                summary0Style:null,
+                summary1Style:null,
+                main1titleStyle:null,
+                main0titleStyle:null,
+                data0Desc0Stype:null,
+                data0Desc1Stype:null,
+                data0Desc2Stype:null,
+                data0Desc3Stype:null,
+                data0Desc4Stype:null,
+                data0TitleStype:null,
+                data1TitleStype:null,
+                data0PieStype:null,
+                data0TextStype:null,
+                data1PieStype:null,
+                data1TextStype:null,
+                data1Desc0Stype:null,
+                data1Desc1Stype:null,
+                data0BgStyle:null,
+                data1BgStyle:null,
+
+                type0Text3Style:null,
+                type0Flag3Style:null,
+                type0Text2Style:null,
+                type0Flag2Style:null,
+                type0Text1Style:null,
+                type0Flag1Style:null,
+                type0Text0Style:null,
+                type0Flag0Style:null,
+
                 pieData0:[
                     {range:[0,0.03],color:'#dec094',},
                     {range:[0.03,0.22],color:'#9f1e23',},
@@ -96,10 +410,6 @@ import Pie from './lib/pie.ts';
         mounted(){
             pie0=new Pie(this.$refs.canvas0);
             pie1=new Pie(this.$refs.canvas1);
-            pie0.render({
-                progress:1,
-                data:this.pieData0
-            });
             pie1.render({
                 progress:1,
                 data:this.pieData1
@@ -139,6 +449,7 @@ import Pie from './lib/pie.ts';
                 left:41.935484%;
                 top:44.4%;
                 background:url("../assets/text.ps-68.png") no-repeat center;
+                transform-origin: left center;
             }
             .ps-137{
                 position:absolute;
@@ -242,6 +553,12 @@ import Pie from './lib/pie.ts';
                         left:50%;
                         top:66.443595%;
                         background:url("../assets/bg.ps-180.png") no-repeat center;
+                        background-image: none;
+                        .bg{
+                            position: absolute;
+                            top: 0;left: 0;
+                            background:url("../assets/bg.ps-180.png") no-repeat center;
+                        }
                         .ps-178{
                             position:absolute;
                             left:42.421875%;
@@ -302,6 +619,12 @@ import Pie from './lib/pie.ts';
                         left:50%;
                         top:57.696759%;
                         background:url("../assets/bg.ps-202.png") no-repeat center;
+                        background-image: none;
+                        .bg{
+                            position: absolute;
+                            top: 0;left: 0;
+                            background:url("../assets/bg.ps-202.png") no-repeat center;
+                        }
                         .ps-193{
                             position:absolute;
                             left:42.421875%;
